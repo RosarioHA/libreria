d5a15ed8896abb675f24e6cebf461e5cd84b651b
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2023-05-18 11:38:35 -0400

    crea ruta

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
 import Home from './components/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Home/>
+    <><BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Home/>} />
+      </Routes>
+    </BrowserRouter><Home /></>
    
   );
 }
