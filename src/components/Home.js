7c1c6b79bc823bef134db8738d9d1f35c4bd3d28
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2023-05-18 12:06:57 -0400

    da estilo a tarjetas de libros

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,15 @@ import React from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import Books from "./Books";
+import "../styles/home.css";
 
 function Home() {
     return(
      <>
      <Navbar/>
+     <div id="cardContainer">
      <Books />
+     </div>
      <Footer />
      </>
     )
